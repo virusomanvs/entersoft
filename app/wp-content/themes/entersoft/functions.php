5dc342656b513986fa04fd192a3a9fda9567fbb3
virusomanvs <virusomanvs@gmail.com>
2018-03-04 20:01:23 +0600

    - fix br

--- app/wp-content/themes/entersoft/functions.php
+++ app/wp-content/themes/entersoft/functions.php
@@ -178,3 +178,8 @@ function my_search_form( $form ) {
 
     return $form;
 }
+
+/** Отключаем автоформатирование */
+remove_filter( 'the_content', 'wpautop' );// для контента
+remove_filter( 'the_excerpt', 'wpautop' );// для анонсов
+remove_filter( 'comment_text', 'wpautop' );// для комментарий
